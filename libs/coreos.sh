61ee38498733b8e0ae35d7174f5ec899b86d83f1
chinya.huang <chinyahuang8@gmail>
2018-03-02 13:48:20 +0800

    create coreos

---
+++ libs/coreos.sh
@@ -0,0 +1,14 @@
+. ./cmd.sh
+
+sudo coreos-install -d /dev/sda -C stable -i cloud-config-file
+
+function install_coreos() {
+  local _conf=$1
+  local _extension=`cut -d'.' -f2`
+
+  [[ ${_extension} == 'json' ]] && \
+  run_and_validate_cmd "sudo coreos-install -d /dev/sda -C stable -i ${_conf}"
+
+  [[ ${_extension} == 'yaml' ]] && \
+  run_and_validate_cmd "sudo coreos-install -d /dev/sda -C stable -c ${_conf}"
+}

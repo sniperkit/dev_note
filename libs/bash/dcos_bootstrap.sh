48087136be56e860ac4fe22655f60f1b0a3f87f6
chinya.huang <chinyahuang8@gmail>
2018-03-13 13:18:03 +0800

    - fix create_ip_detect_script - add uninstall_dcos - add create dcos config

--- libs/bash/dcos_bootstrap.sh
+++ libs/bash/dcos_bootstrap.sh
@@ -1,35 +1,42 @@
 #!/usr/bin/env bash
 
 . ./file_and_dir.sh
+. ./sed.sh
 
-#DEFAULT_REMOTE_UNIVERSE='universe.mesosphere.com/repo'
+##DEFAULT_REMOTE_UNIVERSE='universe.mesosphere.com/repo'
+#
+#EXEC_HOME='/usr/local/bin'
+#
+#CLI_DOWNLOAD='https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.10/dcos'
+#
+#UNIVERSAL_SRV_DOWNLOAD='https://github.com/mesosphere/universe.git --branch version-3.x'
+#UNIVERSAL_SRV_SRC="${SRC_HOME}/universal_server"
+#
+#UNIVERSE_HTTP='dcos-local-universe-http'
+#UNIVERSE_HTTP_DOWNLOAD="https://raw.githubusercontent.com/mesosphere/universe/version-3.x/docker/local-universe/${UNIVERSE_HTTP}.service"
+#
+#UNIVERSE_REGISTRY='dcos-local-universe-registry'
+#UNIVERSE_REGISTRY_DOWNLOAD="curl -v https://raw.githubusercontent.com/mesosphere/universe/version-3.x/docker/local-universe/${UNIVERSE_REGISTRY}.service"
+#
+#UNIVERSE_NODE='192.168.201.108'
+#MASTER_NODE='192.168.201.101'
 
-EXEC_HOME='/usr/local/bin'
+function download_dcos_generate_config {
+  local _dest=$1
 
-CLI_DOWNLOAD='https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.10/dcos'
+  create_dir "${_dest}"
+  curl -o ${_dest}/dcos_generate_config.sh https://downloads.dcos.io/dcos/stable/dcos_generate_config.sh
+}
 
-UNIVERSAL_SRV_DOWNLOAD='https://github.com/mesosphere/universe.git --branch version-3.x'
-UNIVERSAL_SRV_SRC="${SRC_HOME}/universal_server"
-
-UNIVERSE_HTTP='dcos-local-universe-http'
-UNIVERSE_HTTP_DOWNLOAD="https://raw.githubusercontent.com/mesosphere/universe/version-3.x/docker/local-universe/${UNIVERSE_HTTP}.service"
-
-UNIVERSE_REGISTRY='dcos-local-universe-registry'
-UNIVERSE_REGISTRY_DOWNLOAD="curl -v https://raw.githubusercontent.com/mesosphere/universe/version-3.x/docker/local-universe/${UNIVERSE_REGISTRY}.service"
-
-UNIVERSE_NODE='192.168.201.108'
-MASTER_NODE='192.168.201.101'
-
-# create ip-detect.sh
 function create_ip_detect_script {
   local _route_dest=$1
-  local _script=$2
+  local _script="$2/ip-detect"
 
   local _content=`cat << EOF
 #!/usr/bin/env bash
 export LANG=C.UTF-8
 
-release=`uname -r`
+release="\`uname -r\`"
 
 # aws private
 # curl -fsL http://169.254.169.254/latest/meta-data/local-ipv4 && echo && exit 0
@@ -37,14 +44,67 @@ release=`uname -r`
 ##  curl -fsL http://instance-data/latest/meta-data/public-ipv4 && echo && exit 0
 ## curl -fsL http://169.254.169.254/latest/meta-data/public-ipv4 && echo && exit 0
 
-[[ \$release = *"coreos"* ]] && ip route get ${_route_dest} | awk '{print \$5; exit}' && exit 0
+[[ \\$release = *"coreos"* ]] && ip route get ${_route_dest} | awk '{print \\$5; exit}' && exit 0
 
-ip route get ${_route_dest} | awk '{print \$6; exit}' && exit 0
+ip route get ${_route_dest} | awk '{print \\$6; exit}' && exit 0
 
 EOF`
 
+  create_dir "$2"
   overwrite_content "${_content}" "${_script}"
 }
+
+function uninstall_dcos {
+  for i in `find /* -name *dcos*`; do echo $i | grep -P '\bdcos' | xargs rm -rf ; done
+  for i in `find /* -name *_dcos*`; do echo $i | xargs rm -rf ; done
+  for i in `find /* -name *mesos*`; do echo $i | grep -P '\bmesos' | xargs rm -rf ; done
+  for i in `find /* -name *_mesos*`; do echo $i | xargs rm -rf ; done
+}
+
+function create_dcos_config {
+  local _bootstrap_host=$2
+  local _bootstrap_port=$3
+
+  declare -a _master_list=("${!4}")
+
+  local _cluster_name="dcos-test"
+  local _ssh_user="admin"
+
+  local _config="$1/config.yaml"
+  local _content=`cat << EOF
+---
+bootstrap_url: http://${_bootstrap_host}:${_bootstrap_port}
+cluster_name: ${_cluster_name}
+exhibitor_storage_backend: static
+master_discovery: static
+ip_detect_public_filename: genconf/ip-detect
+master_list:
+resolvers:
+- 8.8.8.8
+- 8.8.4.4
+ssh_port: 22
+ssh_user: ${_ssh_user}
+process_timeout: 10000
+oauth_enabled: 'false'
+
+EOF`
+
+  overwrite_content "${_content}" "${_config}"
+
+  for master in ${_master_list[@]}; do
+    echo ${master}
+    add_line_after_match "^master_list:" "- ${master}" "${_config}"
+  done
+}
+
+DCOS_BOOTSTRAP_HOME="/opt/dcos_bootstrap"
+BOOTSTRAP_HOST=$1
+BOOTSTRAP_PORT="10080"
+MASTER_LIST=("192.168.201.108" "192.168.201.109")
+
+#download_dcos_generate_config "${DCOS_BOOTSTRAP_HOME}"
+#create_ip_detect_script "${BOOTSTRAP_HOST}" "${DCOS_BOOTSTRAP_HOME}/genconf"
+create_dcos_config "${DCOS_BOOTSTRAP_HOME}/genconf" "${BOOTSTRAP_HOST}" "${BOOTSTRAP_PORT}" "MASTER_LIST[@]"
 
 # create config.yaml
 # start nginx and map port 10080:80
--- libs/bash/sed.sh
+++ libs/bash/sed.sh
@@ -17,3 +17,15 @@ function escape_forward_slash() {
 
   echo ${_input} | sed 's/\//\\\//g' && return 0 || return 1
 }
+
+function add_line_after_match() {
+  local _find=$1
+  local _add_line=$2
+  local _file=$3
+  local _sudo=${4:-}
+
+  echo "sed -E -i 's/${_find}/& \n${_add_line}/' ${_file}"
+  ${_sudo} sed -E -i "s/${_find}/& \n${_add_line}/" ${_file} && \
+  log "${FONT_GREEN} ... success${FONT_NORMAL}" "[SED][add_line]" || \
+  log "${FONT_RED} ... failed${FONT_NORMAL}" "[SED][add_line]"
+}

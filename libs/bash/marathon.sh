0f9c6a7fd3a6d0b04fbe8782e349569a8814701b
chinya.huang <chinyahuang8@gmail>
2018-03-14 15:21:51 +0800

    - add marathon_get_pods - add marathon_delete_pods - moved coreos templates under templates - create marathon definition json samples

--- libs/bash/marathon.sh
+++ libs/bash/marathon.sh
@@ -16,4 +16,18 @@ function marathon_deploy_pods {
   curl -X POST http://${_host}/v2/pods -d @${_json} -H "Content-type: application/json"
 }
 
+function marathon_get_pods {
+  local _host=$1
+  local _pod=$2
+
+  curl -X GET http://${_host}/v2/pods/${_pod}::status
+}
+
+function marathon_delete_pods {
+  local _host=$1
+  local _pod=$2
+
+  curl -X DELETE http://${_host}/v2/pods/${_pod}
+}
+
 marathon_deploy_app "192.168.201.108:8080" "/tmp/marathon/sample.json"

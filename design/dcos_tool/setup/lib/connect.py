eb83196caff9685296e8c01530bc48f620a7645f
chinya.huang <chinyahuang8@gmail>
2018-03-27 09:00:02 +0800

    - add master deployment

--- design/dcos_tool/setup/lib/connect.py
+++ design/dcos_tool/setup/lib/connect.py
@@ -3,15 +3,38 @@
 import subprocess
 import time
 import os
+import paramiko
 from .log import Log
 from .log import LogNormal, LogError
 
 from .utils import split_lines
 
 
+class SshSession():
+    def __init__(self, dest_host, dest_user, dest_password):
+        self.host=str(dest_host)
+        self.username=str(dest_user)
+        self.password=str(dest_password)
+        self.log=Log()
+
+    def login_with_password(self):
+        p_ssh = paramiko.SSHClient()
+        p_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            p_ssh.connect(hostname=self.host, username=self.username, password=self.password)
+            self.log.output(log_message='connect', header='[SESSION][ssh]', show_state='ok')
+
+            return p_ssh
+
+        except (paramiko.ssh_exception.SSHException, paramiko.ssh_exception.NoValidConnectionsError) as e:
+            self.log.output(log_message='connect', header='[SESSION][ssh]', show_state='error')
+            self.log.output(log_message=str(e), header='[SESSION][error]')
+
+            return None
+
+
 class Shell():
     def __init__(self, session=None):
-        self.remote = False
         self.session = session.invoke_shell() if session is not None else None
 
         self.retry_expire = int('60')
@@ -42,13 +65,21 @@ class Shell():
             LogNormal(NORM_LOCAL_CMD_RETURN={ "cmd": command })
 
     def remote(self, command):
-        self.session.send(command + split_lines)
-        self.log.output(log_message=command, header="[SHELL][remote]", show_state='complete')
+        chn = self.session.get_transport().open_session()
 
-        timeout = round(time.time()) + self.retry_expire
-        while time.time() < timeout:
+        chn.settimeout(10800)
+        chn.exec_command(command)
+        LogNormal(NORM_REMOTE_CMD_RETURN={"cmd": command})
+
+        while not chn.exit_status_ready():
             time.sleep(self.retry_interval)
 
-            if self.session.recv_ready():
-                out = '\n'.join(split_lines(input_byte=self.session.recv(1024)))
-                self.log.output(log_message=out, header="[SHELL][remote]")
+            if chn.recv_ready():
+                data_buffer = '\n'.join(split_lines(input_byte=chn.recv(1024)))
+
+                while data_buffer:
+                    LogNormal(STDOUT={"stdout": data_buffer})
+                    data_buffer = '\n'.join(split_lines(input_byte=chn.recv(1024)))
+
+        if chn.recv_exit_status() is not 0:
+            LogError(BAD_REMOTE_CMD_RETURN={"cmd": command, "return_code": chn.recv_exit_status()})
--- design/dcos_tool/setup/lib/log.py
+++ design/dcos_tool/setup/lib/log.py
@@ -33,12 +33,14 @@ class LogError:
         self.log = Log()
         self.stderr = kwargs.get('STDERR', None)
         self.bad_local_cmd_return = kwargs.get('BAD_LOCAL_CMD_RETURN', None)
+        self.bad_remote_cmd_return = kwargs.get('BAD_REMOTE_CMD_RETURN', None)
 
         self.msg = None
         self.header = "[ERROR]"
         self.show_state = "error"
 
         self._shell()
+        self._remote_shell()
         self._stderr()
 
         self.log.output(log_message=self.msg, header=self.header, show_state=self.show_state)
@@ -54,27 +56,48 @@ class LogError:
             self.msg = self.bad_local_cmd_return.get("cmd")
             self.header = "[SHELL][local]<return_code: {}>".format(self.bad_local_cmd_return.get("return_code"))
 
+    def _remote_shell(self):
+        if self.bad_remote_cmd_return is not None:
+            self.msg = self.bad_remote_cmd_return.get("cmd")
+            self.header = "[SHELL][remote]<return_code: {}>".format(self.bad_remote_cmd_return.get("return_code"))
+
 
 class LogNormal:
     def __init__(self, **kwargs):
         self.log = Log()
 
+        self.stdout = kwargs.get('STDOUT', None)
+
         self.template_create_new = kwargs.get('NORM_CREATE_TEMPLATE', None)
         self.local_cmd_return = kwargs.get('NORM_LOCAL_CMD_RETURN', None)
+        self.remote_cmd_return = kwargs.get('NORM_REMOTE_CMD_RETURN', None)
 
         self.msg = None
         self.header = "[NORMAL]"
         self.show_state = "normal"
 
+        self._stdout()
         self._shell()
+        self._remote_shell()
         self._template()
 
         self.log.output(log_message=self.msg, header=self.header, show_state=self.show_state)
+
+    def _stdout(self):
+        if self.stdout is not None:
+            self.msg = self.stdout.get("stdout")
+            self.header = "[STDOUT]"
+            self.show_state = ''
 
     def _shell(self):
         if self.local_cmd_return is not None:
             self.msg = self.local_cmd_return.get("cmd")
             self.header = "[SHELL][local]"
+
+    def _remote_shell(self):
+        if self.remote_cmd_return is not None:
+            self.msg = self.remote_cmd_return.get("cmd")
+            self.header = "[SHELL][remote]"
 
     def _template(self):
         if self.template_create_new is not None:
